575a255ee35a63ad9a373ffeb12794fa60b6d89d
anton.kovalev <camotank12345@gmail.com>
2022-09-20 03:41:21 +0300

    added primitive component

--- src/engine/components.h
+++ src/engine/components.h
@@ -2,8 +2,10 @@
 
 namespace ark::entities
 {
-	class p
+	struct primitive_component
 	{
-
+		bool is_collidable = false;
+		bool is_drawable = true;
+		std::vector<ark_int_vec2> points;
 	};
 }
--- src/engine/types.h
+++ src/engine/types.h
@@ -1,1 +1,14 @@
 ﻿#pragma once
+
+struct ark_int_vec2
+{
+	union
+	{
+		struct
+		{
+			int16 x;
+			int16 y;
+		};
+		int16 xy[2];
+	};
+};
