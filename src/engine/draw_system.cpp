543c414a1dcbf5d6bf2cab912531083d2faa66dd
anton.kovalev <camotank12345@gmail.com>
2022-10-04 21:54:57 +0300

    stability fixes

--- src/engine/draw_system.cpp
+++ src/engine/draw_system.cpp
@@ -52,42 +52,44 @@ draw_system::tick(registry& reg, float dt)
 	
 	for (auto& entity : draw_view) {
 		// First of all, try to draw physics bodies
-		if (registry.all_of<physics_body_component>(entity)) {
-			const auto phys_body = registry.try_get<physics_body_component>(entity);
-			if (phys_body == nullptr) {
-				continue;
-			}
-			
-			ark_assert(phys_body->body != nullptr, "phys body can't be null!", continue)
-
-			const auto physical_body = phys_body->body;
-			if (!physical_body->is_created()) {
-				continue;
-			}
-			
-			// Object is not created yet, skip it until it was created
-			if (physical_body->get_body() == nullptr || !physical_body->get_body()->IsEnabled()) {
-				continue;
-			}
-
-			// If we don't have any of draw components - try to draw physics body with debug view
-			if (!registry.any_of<draw_color_component, draw_gradient_component, draw_texture_component>(entity)) {
-				const auto phys_body_id = reinterpret_cast<ptrdiff_t>(physical_body);
-				if (!color_map.contains(phys_body_id)) {
-					std::uniform_int_distribution color_dist(55, 255);
-					const int red_color = color_dist(r_engine);
-					const int green_color = color_dist(r_engine);
-					const int blue_color = color_dist(r_engine);
-					color_map.insert(std::make_pair(phys_body_id, ImColor(red_color, green_color, blue_color)));
+		if (entities::is_valid(entity)) {
+			if (registry.all_of<physics_body_component>(entity)) {
+				const auto phys_body = registry.try_get<physics_body_component>(entity);
+				if (phys_body == nullptr) {
+					continue;
 				}
-			
-				if (physical_body->get_body()->GetFixtureList()->GetType() == b2Shape::Type::e_circle) {
-					graphics::draw_physical_circle_object(physical_body->get_body(), color_map[phys_body_id]);
-				} else {
-					graphics::draw_physical_object(physical_body->get_body(), color_map[phys_body_id]);
+
+				ark_assert(phys_body->body != nullptr, "phys body can't be null!", continue)
+
+				const auto physical_body = phys_body->body;
+				if (!physical_body->is_created()) {
+					continue;
 				}
-			
-				continue;
+
+				// Object is not created yet, skip it until it was created
+				if (physical_body->get_body() == nullptr || !physical_body->get_body()->IsEnabled()) {
+					continue;
+				}
+
+				// If we don't have any of draw components - try to draw physics body with debug view
+				if (!registry.any_of<draw_color_component, draw_gradient_component, draw_texture_component>(entity)) {
+					const auto phys_body_id = reinterpret_cast<ptrdiff_t>(physical_body);
+					if (!color_map.contains(phys_body_id)) {
+						std::uniform_int_distribution color_dist(55, 255);
+						const int red_color = color_dist(r_engine);
+						const int green_color = color_dist(r_engine);
+						const int blue_color = color_dist(r_engine);
+						color_map.insert(std::make_pair(phys_body_id, ImColor(red_color, green_color, blue_color)));
+					}
+
+					if (physical_body->get_body()->GetFixtureList()->GetType() == b2Shape::Type::e_circle) {
+						graphics::draw_physical_circle_object(physical_body->get_body(), color_map[phys_body_id]);
+					} else {
+						graphics::draw_physical_object(physical_body->get_body(), color_map[phys_body_id]);
+					}
+
+					continue;
+				}
 			}
 		}
 	}
--- src/engine/physics_system.cpp
+++ src/engine/physics_system.cpp
@@ -21,17 +21,19 @@ physics_system::tick(registry& reg, float dt)
 	entt::registry& registry = reg.get();
 	const auto view = registry.view<entities::physics_body_component>();
 	for (const auto entity : view) {
-		auto phys_body = registry.try_get<entities::physics_body_component>(entity);
-		auto scene_comp = registry.try_get<entities::scene_component>(entity);
-		if (phys_body != nullptr && phys_body->body != nullptr) {
-			const auto& pos = phys_body->body->get_position();
-			if (scene_comp != nullptr) {
-				scene_comp->position = pos;
-			}
+		if (entities::is_valid(entity)) {
+			auto phys_body = registry.try_get<entities::physics_body_component>(entity);
+			auto scene_comp = registry.try_get<entities::scene_component>(entity);
+			if (phys_body != nullptr && phys_body->body != nullptr) {
+				const auto& pos = phys_body->body->get_position();
+				if (scene_comp != nullptr) {
+					scene_comp->position = pos;
+				}
 
-			if (pos.y < y_destroy_coord) {
-				entities::mark_as_garbage(entity);
-				continue;
+				if (pos.y < y_destroy_coord) {
+					entities::mark_as_garbage(entity);
+					continue;
+				}
 			}
 		}
 	}
