7ab92f428406e20d084fcbdd1e53042dcb064df8
ForserX <sv3nk@yandex.ru>
2022-11-24 01:25:12 +0300

    1

--- src/engine/audio/module_OpenAL.cpp
+++ src/engine/audio/module_OpenAL.cpp
@@ -9,6 +9,8 @@
 
 using namespace ark;
 
+static bool al_work = false;
+
 namespace al_trash
 {
 #	include "module_OpenAL.inl"
@@ -18,12 +20,16 @@ static stl::vector<al_trash::stream_audio_data*> data;
 
 void audio::openal::init()
 {
-	al_trash::init_al();
+	al_work = !al_trash::init_al();
 	data = {};
 }
 
 void audio::openal::tick()
 {
+	if (!al_work) {
+		return;
+	}
+
 	for (al_trash::stream_audio_data* it : data)
 	{
 		bool playing = al_trash::update_stream(*it);
@@ -39,18 +45,24 @@ void audio::openal::tick()
 
 void audio::openal::destroy()
 {
+	if (!al_work) {
+		return;
+	}
+
 	for (al_trash::stream_audio_data* it : data)
 	{
 		delete it;
 	}
 
 	data.clear();
-
 	al_trash::CloseAL();
 }
 
 void audio::openal::start(stl::string_view sound_src)
 {
+	if (!al_work) {
+		return;
+	}
 	al_trash::stream_audio_data& ref_data = *data.emplace_back(new al_trash::stream_audio_data);
 	al_trash::create_stream_from_file(sound_src, ref_data);
 	
