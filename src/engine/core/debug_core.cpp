38b03999793f9b0b1752c3cfb5252c7b8dd95eaf
ForserX <sv3nk@yandex.ru>
2022-12-10 18:05:12 +0300

    Debug update OSX & Linux

--- src/engine/core/debug_core.cpp
+++ src/engine/core/debug_core.cpp
@@ -1,14 +1,98 @@
 #include "pch.h"
 
-#ifdef OS_MACOS
-#define DebugBreak __builtin_trap
-#elif defined(OS_UNIX)
-#define DebugBreak
-#endif
-
 static std::ofstream log_file;
 
 using namespace asura;
+
+#ifdef OS_APPLE_SERIES
+#define DebugBreak __builtin_trap
+
+#include <assert.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/sysctl.h>
+
+bool IsDebuggerPresent()
+{
+    int mib[4];
+    struct kinfo_proc info;
+    size_t size;
+
+    info.kp_proc.p_flag = 0;
+    mib[0] = CTL_KERN;
+    mib[1] = KERN_PROC;
+    mib[2] = KERN_PROC_PID;
+    mib[3] = getpid();
+
+    size = sizeof(info);
+    sysctl(mib, sizeof(mib) / sizeof(*mib), &info, &size, NULL, 0);
+
+    return ((info.kp_proc.p_flag & P_TRACED) != 0);
+}
+
+void OutputDebugString(const char* data)
+{
+    if (!IsDebuggerPresent())
+        return;
+
+	std::cout << "Asura Engine: " << data << std::endl;
+}
+#elif defined(OS_LINUX)
+bool IsDebuggerPresent()
+{
+    int pid = fork();
+    int res = 0;
+
+    if (pid == -1)
+    {
+        perror("fork");
+        return -1;
+    }
+
+    if (pid == 0)
+    {
+        int ppid = getppid();
+
+        /* Child */
+        if (ptrace(PTRACE_ATTACH, ppid, NULL, NULL) == 0)
+        {
+            /* Wait for the parent to stop and continue it */
+            waitpid(ppid, NULL, 0);
+            ptrace(PTRACE_CONT, NULL, NULL);
+
+            /* Detach */
+            ptrace(PTRACE_DETACH, getppid(), NULL, NULL);
+
+            /* We were the tracers, so gdb is not present */
+            res = 0;
+        }
+        else
+        {
+            /* Trace failed so GDB is present */
+            res = 1;
+        }
+        exit(res);
+    }
+    else
+    {
+        int status;
+        waitpid(pid, &status, 0);
+        res = WEXITSTATUS(status);
+    }
+    return !!res;
+}
+
+void DebugBreak()
+{
+	BYTE bCrash = *(BYTE*)(nullptr);
+}
+
+void OutputDebugString(const char* data)
+{
+    std::cout << "Asura Engine: " << data << std::endl;
+}
+#endif
 
 void debug::init()
 {
@@ -42,12 +126,11 @@ void debug::print_message(stl::string_view message)
 
 #if defined(_DEBUG) & defined(OS_WINDOWS)
 	if (IsDebuggerPresent()) {
-		OutputDebugString("asura: ");
+		OutputDebugString("Asura Engine: ");
 		OutputDebugString(message.data());
 		OutputDebugString("\r\n");
 	}
-#endif
-#if defined(OS_LINUX)
-	std::cout << "asura: " << message.data() << std::endl;	
+#else
+    OutputDebugString(message.data());
 #endif
 }
