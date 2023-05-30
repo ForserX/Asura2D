7e1450d2a0529bb8e70435c2a9d21ae6325bac3e
ForserX <sv3nk@yandex.ru>
2023-05-30 19:40:47 +0300

    Restored old testapp code

--- src/game/ingame.cpp
+++ src/game/ingame.cpp
@@ -47,11 +47,55 @@ public:
 		AddTexture(Ent, "textures/ball.png", false);
 	};
 };
+
+EntityView TestObject2;
+class ContactLister : public Physics::ContatctListerBase
+{
+public:
+	virtual void BeginContact(b2Contact* Contact) override
+	{
+		static stl::uniform_dist_t<int16_t> r_dist(1, 7);
+
+		auto BodyA = Physics::PhysicsBody(Contact->GetFixtureA()->GetBody());
+		auto BodyB = Physics::PhysicsBody(Contact->GetFixtureB()->GetBody());
+
+		auto TryEntt = Entities::TryGet<Entities::physics_body_component>(TestObject2.Get());
+		if (TryEntt == nullptr)
+			return;
+
+		auto& TryEnttBody = *TryEntt->body;
+		if (BodyA == TryEnttBody)
+		{
+			// Destroy dynamic objects only
+			if (BodyB.GetType() == Physics::BodyType::Dynamic)
+			{
+				std::string Name = "ball\\ball_blob_rr_0" + std::to_string(r_dist(gen));
+				Name += ".opus";
+
+				Audio::Start(Name);
+				Physics::SafeFree(new Physics::PhysicsBody(std::move(BodyB)));
+			}
+		}
+		else if (BodyB == TryEnttBody)
+		{
+			// Destroy dynamic objects only
+			if (BodyA.GetType() == Physics::BodyType::Dynamic)
+			{
+				std::string Name = "ball\\ball_blob_rr_0" + std::to_string(r_dist(gen));
+				Name += ".opus";
+
+				Audio::Start(Name);
+
+				Physics::SafeFree(new Physics::PhysicsBody(std::move(BodyA)));
+			}
+		}
+	}
+};
 #endif
 
 void init_systems()
 {
-//	game_update_systems.emplace_back(std::make_unique<RandomSys>());
+	game_update_systems.emplace_back(std::make_unique<RandomSys>());
 }
 
 void ingame::pre_init()
@@ -99,8 +143,6 @@ auto editor_key_change = [](int16_t scan_code, Input::State state)
 
 int64_t editor_key_event;
 
-EntityView TestObject2;
-
 void ingame::init()
 {
 	using namespace Asura;
@@ -109,14 +151,21 @@ void ingame::init()
 	size_t CursorID = UI::CursorManager::Register("textures/cursor/base.png");
 	UI::CursorManager::SetActive(CursorID);
 
+#if 0
 	TestObject2 = CreatePhysBody(Physics::body_parameters(0.f, 0.f, {}, { 550, 300 }, { 60, 100 }));
 	AddField<drawable_flag>(TestObject2);
 	AddField<draw_texture_npc_component>(TestObject2);
-
 	AddTexture(TestObject2, "textures/npc_test.png", false);
 	holder_type = GamePlay::holder_mode::Player;
 	GamePlay::Holder::Player::Bind(TestObject2.Get());
+#else 
+	Physics::GetWorld().SetContactLister(new ContactLister);
+	TestObject2 = CreatePhysBody(Physics::body_parameters(0.f, 0.f, {}, { 350, 100 }, { 100, 10 }));
+	AddField<drawable_flag>(TestObject2);
 
+	holder_type = GamePlay::holder_mode::PlayerFree;
+	GamePlay::Holder::PlayerFree::Bind(TestObject2.Get());
+#endif
 	editor_key_event = Input::Emplace(editor_key_change);
 }
 
